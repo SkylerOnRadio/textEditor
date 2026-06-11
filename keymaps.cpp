b7e22692faf50554c8631cf3a4694f1536b36553
SkylerOnRadio <abhigyadulal@gmail.com>
2026-06-11 21:03:01 +0530

    redid move right function to not be able to enter characters after the newline character, ad not to be able to go to the next line if the next line does not exist

--- keymaps.cpp
+++ keymaps.cpp
@@ -5,11 +5,14 @@
 
 void moveCursorDown(int &cursorY, int &cursorX,
                     std::vector<std::unique_ptr<Line>> &lines) {
-  if (cursorY == Line::noOfLines)
+  // if the cursor is at the last line or beyond that (somehow) return
+  if (cursorY >= Line::noOfLines)
     return;
 
   cursorY++;
   Line *lineToGoTo = lines.at(cursorY - 1).get();
+  // check if the line has newLine, if yes then set the maximum X coord such
+  // that it ignore the newLine character
   int maxXCord = lineToGoTo->end->letter == '\n' ? lineToGoTo->characters
                                                  : lineToGoTo->characters + 1;
   if (cursorX > maxXCord)
@@ -23,6 +26,8 @@ void moveCursorLeft(int &cursorY, int &cursorX,
       return;
 
     cursorY--;
+    // put the cursor's X co-ordinate right where the newLine character is, so
+    // writing new characters still means that the newLine is at the last
     cursorX = lines.at(cursorY - 1)->characters;
     return;
   }
@@ -34,8 +39,9 @@ void moveCursorRight(int &cursorY, int &cursorX,
   Line *line = lines.at(cursorY - 1).get();
   int maxXCord =
       line->end->letter == '\n' ? line->characters : line->characters + 1;
-  if (cursorX >= maxXCord) {
-    if (Line::noOfLines > cursorY - 1) {
+  // if the next x position is greater or equal to the maximum X co-ordinate
+  if (cursorX + 1 > maxXCord) {
+    if (cursorY < Line::noOfLines) {
       cursorY++;
       cursorX = 1;
       return;
